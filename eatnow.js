a4af34b94fa9143e24413f21cfcf8de66a5eed23
a1b2c3-rgb <violettalemwa2@gmail.com>
2025-02-26 15:24:46 +0300

    Create eatnow.js

---
+++ eatnow.js
@@ -0,0 +1,23 @@
+let userName = prompt("Welcome to EatNow Cafe! Please enter your first name:");
+if (!userName) {
+    userName = "Guest";
+}
+
+alert('Hello ${userName}, welcome to EatNow Cafe! We hope you have a great experience.');
+
+let menuItems = ["Grilled Chicken", "Veggie Burger", "Pasta Alfredo", "BBQ Ribs", "Caesar Salad"];
+console.log("Here is our menu:");
+menuItems.forEach((item, index) => {
+    console.log('${index + 1}. ${item}');
+});
+
+let newDish = prompt("Would you like to suggest a new dish to add to our menu?");
+if (newDish !== null && newDish.trim() !== "") {
+    menuItems.push(newDish.trim());
+    console.log("Updated menu:");
+    menuItems.forEach((item, index) => {
+        console.log('${index + 1}. ${item}');
+    });
+} else {
+    console.log("No new dish added.");
+}
